997228953de80dfe39821a0fde956fff794022fd
armin bakhshi <arminbkh0921@yahoo.com>
2022-01-08 17:27:20 +0330

    push user to home page after three second delay in 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const NotFound = () => {
+    const router = useRouter();
+    
+    useEffect(()=> {
+        setTimeout(()=> router.push({ pathname: "/", search: "HEY=Hey" }), 3000)
+    }, [])
+
   return (
     <main>
       <Head>
@@ -11,7 +19,7 @@ const NotFound = () => {
       <h2>not found</h2>
       <p>
         go back to{" "}
-        <Link href="/">
+        <Link href={{ pathname: "/", query: { HEY: "Hey" } }}>
           <a>home page</a>
         </Link>
       </p>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Footer from "../components/Navbar/Footer";
-import Navbar from "../components/Navbar/Navbar";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router);
+  }, []);
   return (
-      <main>
-        <h1>Home page</h1>
-        <p>this is home page guys</p>
-      </main>
+    <main>
+      <h1>Home page</h1>
+      <p>this is home page guys</p>
+    </main>
   );
 }
