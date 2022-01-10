a2070019a95e2f4b3a70dde312a25e57c5a5175e
armin-bkh <arminbkh0921@yahoo.com>
2022-01-10 14:53:40 +0330

    redirect Footer component to Footer folder

--- Layout/Layout.jsx
+++ Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import Footer from "../components/Navbar/Footer";
+import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 
 const Layout = ({ children }) => {
